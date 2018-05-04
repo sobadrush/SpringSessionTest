6db8060b51da1014599c76b0c48112a4b57cdd1d
sobadrush <sobadrush@outlook.com>
2018-05-04 15:39:54 +0800

    @annotation配置測試成功

--- src/main/java/_00_config/RootConfig.java
+++ src/main/java/_00_config/RootConfig.java
@@ -5,36 +5,36 @@ import java.sql.SQLException;
 import javax.sql.DataSource;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.session.jdbc.config.annotation.web.http.EnableJdbcHttpSession;
 
-//@Configuration
+@Configuration
 //@ImportResource("file:**\\WEB-INF\\spring-beans.xml")
 //@EnableTransactionManagement
-//@EnableJdbcHttpSession
+@EnableJdbcHttpSession
 //@ImportResource("file:E:\\CTBC_workspace_Oxygen_3a\\SpringSessionTest\\src\\main\\webapp\\WEB-INF\\spring-beans.xml")
 //@ComponentScan(basePackageClasses = { org.springframework.web.filter.DelegatingFilterProxy.class })
 //@ComponentScan(basePackages= {"org.springframework.web.filter.*"})
 public class RootConfig {
 
-//	@Bean
-//	public DataSource dataSource(){
-//		DriverManagerDataSource ds = new DriverManagerDataSource();
-//		ds.setUrl("jdbc:sqlserver://172.24.17.52:1803;databaseName=ITOA_MAIN");
-//		ds.setDriverClassName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-//		ds.setUsername("ITOA_MAIN_mod");
-//		ds.setPassword("f3ru9cj4");
-//		return ds; 
-//	}
-	
 	@Bean
-    public EmbeddedDatabase embeddedDatabase() {
-            return new EmbeddedDatabaseBuilder() 
-                            .setType(EmbeddedDatabaseType.H2)
-                            .addScript("org/springframework/session/jdbc/schema-h2.sql").build();
-    }
+	public DataSource dataSource(){
+		DriverManagerDataSource ds = new DriverManagerDataSource();
+		ds.setUrl("jdbc:sqlserver://172.24.17.52:1803;databaseName=ITOA_MAIN");
+		ds.setDriverClassName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+		ds.setUsername("ITOA_MAIN_mod");
+		ds.setPassword("f3ru9cj4");
+		return ds; 
+	}
+	
+//	@Bean
+//    public EmbeddedDatabase embeddedDatabase() {
+//            return new EmbeddedDatabaseBuilder() 
+//                            .setType(EmbeddedDatabaseType.H2)
+//                            .addScript("org/springframework/session/jdbc/schema-h2.sql").build();
+//    }
 	
 	@Bean
 	public DataSourceTransactionManager transactionManager(DataSource ds) {
